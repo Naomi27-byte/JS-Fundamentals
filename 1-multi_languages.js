7de467f7a506e51b3d0cc310d7fbfa3257e23191
Orji Naomi <Orjinaomi92@gmail.com>
2025-07-11 19:07:57 +0100

    A script that prints 3 lines

---
+++ 1-multi_languages.js
@@ -0,0 +1,6 @@
+let FirstLine = "C is fun";
+let SecondLine = "Python is cool";
+let ThirdLine = "JavaScript is amazing";
+console.log (FirstLine);
+console.log (SecondLine);
+console.log (ThirdLine);
